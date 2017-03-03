fb923393fb9cd23a025c6c265f143b07df18b9fa
robthez <robertzwetsloot@gmail.com>
2017-03-03 17:14:25 +0000

    Bug fix

--- ZeroSourceValues/source_values.py
+++ ZeroSourceValues/source_values.py
@@ -2,12 +2,12 @@ from gpiozero import LED, Button
 from gpiozero.tools import all_values, any_values, negated
 from signal import pause
 
-in_1 = Button(0)
-in_2 = Button(1)
+in_1 = Button(23)
+in_2 = Button(15)
 
-out_1 = LED(2)
-out_2 = LED(3)
-out_3 = LED(4)
+out_1 = LED(24)
+out_2 = LED(18)
+out_3 = LED(14)
 
 out_1.source = all_values(in_1.values, in_2.values)
 out_2.source = any_values(in_1.values, in_2.values)
